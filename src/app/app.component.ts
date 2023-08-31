2f1fd259121bdad4b3222d7bc9f810f5550405f2
Ahmad Wijaya <cibofdev@gmail.com>
2023-08-31 22:15:35 +0700

    update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {WeatherService} from "./services/weather.service";
+import {WeatherData} from "./models/weather.model";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-weather-app';
+export class AppComponent implements OnInit {
+
+  constructor(private weatherService: WeatherService) {
+  }
+
+  weatherData?: WeatherData;
+
+  ngOnInit(): void {
+    this.weatherService.getWeatherData('Jakarta')
+      .subscribe({
+        next: (response) => {
+          this.weatherData = response;
+          console.log(response)
+        }
+      });
+  }
 }
