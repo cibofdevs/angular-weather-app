ade889d1fb55a10e5f5591e50a6a3a7e82e1167d
Ahmad Wijaya <cibofdev@gmail.com>
2023-09-01 16:03:54 +0700

    update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,7 @@
-export const environment = {};
+export const environment = {
+  weatherApiBaseUrl: 'https://weather-api99.p.rapidapi.com/weather',
+  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue: 'ac0f33131emsh9be01139169df17p1ad63cjsn0c78908d2b7b',
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue: 'weather-api99.p.rapidapi.com'
+};
